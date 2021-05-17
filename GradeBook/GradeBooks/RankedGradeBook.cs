38a2730ba23a47b78f2b4f0d91fbcb16868f1178
Lo <lohedengran@gmail.com>
2021-05-17 12:55:13 +0200

    small fix

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -39,7 +39,10 @@ namespace GradeBook.GradeBooks
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
+            {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            };
 
             base.CalculateStatistics();
         }
