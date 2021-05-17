9cda7d6851cd59be01c5a377cfd34685d4f21059
Lo <lohedengran@gmail.com>
2021-05-17 13:20:52 +0200

    small fix

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,17 +40,18 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
+            var type = parts[2];
 
             BaseGradeBook gradeBook;
 
 
-            if (parts[2] == "standard")
+            if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
-            else if (parts[2] == "ranked")
+            if (type == "ranked")
                 gradeBook = new RankedGradeBook(name);
             else
             {
-                Console.WriteLine($"{name} is not a supported type of gradebook, please try again");
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
                 return;
             }
 
